2d17d39879eb1061cf2f9a1645d4797c7ea653e5
joshuanwankwo <systemblakes@gmail.com>
2020-11-27 13:55:41 +0100

    Added main file

---
+++ birthRate.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt 
+
+
+births = pd.read_csv("births.csv") 
+print(births.head())
+# print(births['day'])
+#
+
+# births['day'].fillna(0, inplace=True) 
+births= births.replace('null', 0)
+print(births['day'])
+births['day'] = births['day'].astype(int)
+
+births['decade'] = 10 * (births['year'] // 10)
+births.pivot_table('births', index='decade', columns='gender', aggfunc='sum')
+print(births.head())
+ 
+sns.set() 
+birth_decade = births.pivot_table('births', index='decade', columns='gender', aggfunc='sum') 
+birth_decade.plot() 
+plt.ylabel("Total births per year") 
+plt.show()
+
+
+quartiles = np.percentile(births['births'], [25, 50, 75])
+mean = quartiles[1]
+sigma = 0.74 * (quartiles[2] - quartiles[0])
+
+births = births.query('(births > @mean - 5 * @sigma) & (births < @mean + 5 * @sigma)') 
+births.index = pd.to_datetime(10000 * births.year + 100 * births.month + births.day, format='%Y%m%d') 
+births['day of week'] = births.index.dayofweek
+
+births_day = births.pivot_table('births', index='day of week',
+                                columns='decade', aggfunc='mean')
+births_day.index = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun']
+births_day.plot()
+plt.ylabel("Average Births by Day")
+plt.show()
+
+births_month = births.pivot_table('births', [births.index.month, births.index.day])
+print(births_month.head())
+
+births_month.index = [pd.datetime(2012, month, day)
+                      for (month, day) in births_month.index]
+print(births_month.head())
+
+fig, ax = plt.subplots(figsize=(12, 4))
+births_month.plot(ax=ax)
+plt.show()
